d3b4ed8d96ec65a92f6d8ec3a5b1a216eabfba98
William Jennings <JENNINGSW@ETSU.EDU>
2021-11-15 09:25:03 -0500

    Added instruction formats and descriptions and finished form

--- PipeliningSimulation/InstructionReferenceForm.Designer.cs
+++ PipeliningSimulation/InstructionReferenceForm.Designer.cs
@@ -34,18 +34,38 @@ namespace PipeliningSimulation
             this.instructTypeLabel = new System.Windows.Forms.Label();
             this.descLabel = new System.Windows.Forms.Label();
             this.formatLabel = new System.Windows.Forms.Label();
-            this.intructTypeListBox = new System.Windows.Forms.ListBox();
-            this.instructListBox = new System.Windows.Forms.ListBox();
-            this.formatListBox = new System.Windows.Forms.ListBox();
-            this.descriptionListBox = new System.Windows.Forms.ListBox();
             this.closeButton = new System.Windows.Forms.Button();
+            this.intructTypePanel = new System.Windows.Forms.Panel();
+            this.typeListBox3 = new System.Windows.Forms.ListBox();
+            this.typeListBox4 = new System.Windows.Forms.ListBox();
+            this.typeListBox2 = new System.Windows.Forms.ListBox();
+            this.typeListBox1 = new System.Windows.Forms.ListBox();
+            this.instructPanel = new System.Windows.Forms.Panel();
+            this.instructListBox3 = new System.Windows.Forms.ListBox();
+            this.instructListBox4 = new System.Windows.Forms.ListBox();
+            this.instructListBox1 = new System.Windows.Forms.ListBox();
+            this.instructListBox2 = new System.Windows.Forms.ListBox();
+            this.formatPanel = new System.Windows.Forms.Panel();
+            this.formatListBox3 = new System.Windows.Forms.ListBox();
+            this.formatListBox4 = new System.Windows.Forms.ListBox();
+            this.formatListBox1 = new System.Windows.Forms.ListBox();
+            this.formatListBox2 = new System.Windows.Forms.ListBox();
+            this.descPanel = new System.Windows.Forms.Panel();
+            this.descListBox3 = new System.Windows.Forms.ListBox();
+            this.descListBox4 = new System.Windows.Forms.ListBox();
+            this.descListBox2 = new System.Windows.Forms.ListBox();
+            this.descListBox1 = new System.Windows.Forms.ListBox();
+            this.intructTypePanel.SuspendLayout();
+            this.instructPanel.SuspendLayout();
+            this.formatPanel.SuspendLayout();
+            this.descPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // formLabel
             // 
             this.formLabel.AutoSize = true;
             this.formLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.formLabel.Location = new System.Drawing.Point(193, 38);
+            this.formLabel.Location = new System.Drawing.Point(241, 40);
             this.formLabel.Name = "formLabel";
             this.formLabel.Size = new System.Drawing.Size(202, 29);
             this.formLabel.TabIndex = 0;
@@ -55,7 +75,7 @@ namespace PipeliningSimulation
             // 
             this.instructionLabel.AutoSize = true;
             this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.instructionLabel.Location = new System.Drawing.Point(168, 112);
+            this.instructionLabel.Location = new System.Drawing.Point(167, 112);
             this.instructionLabel.Name = "instructionLabel";
             this.instructionLabel.Size = new System.Drawing.Size(67, 16);
             this.instructionLabel.TabIndex = 1;
@@ -75,7 +95,7 @@ namespace PipeliningSimulation
             // 
             this.descLabel.AutoSize = true;
             this.descLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.descLabel.Location = new System.Drawing.Point(416, 112);
+            this.descLabel.Location = new System.Drawing.Point(453, 112);
             this.descLabel.Name = "descLabel";
             this.descLabel.Size = new System.Drawing.Size(76, 16);
             this.descLabel.TabIndex = 3;
@@ -85,48 +105,16 @@ namespace PipeliningSimulation
             // 
             this.formatLabel.AutoSize = true;
             this.formatLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.formatLabel.Location = new System.Drawing.Point(272, 112);
+            this.formatLabel.Location = new System.Drawing.Point(275, 112);
             this.formatLabel.Name = "formatLabel";
             this.formatLabel.Size = new System.Drawing.Size(50, 16);
             this.formatLabel.TabIndex = 4;
             this.formatLabel.Text = "Format";
             // 
-            // intructTypeListBox
-            // 
-            this.intructTypeListBox.FormattingEnabled = true;
-            this.intructTypeListBox.Location = new System.Drawing.Point(43, 133);
-            this.intructTypeListBox.Name = "intructTypeListBox";
-            this.intructTypeListBox.Size = new System.Drawing.Size(105, 316);
-            this.intructTypeListBox.TabIndex = 5;
-            // 
-            // instructListBox
-            // 
-            this.instructListBox.FormattingEnabled = true;
-            this.instructListBox.Location = new System.Drawing.Point(147, 133);
-            this.instructListBox.Name = "instructListBox";
-            this.instructListBox.Size = new System.Drawing.Size(100, 316);
-            this.instructListBox.TabIndex = 6;
-            // 
-            // formatListBox
-            // 
-            this.formatListBox.FormattingEnabled = true;
-            this.formatListBox.Location = new System.Drawing.Point(246, 133);
-            this.formatListBox.Name = "formatListBox";
-            this.formatListBox.Size = new System.Drawing.Size(102, 316);
-            this.formatListBox.TabIndex = 7;
-            // 
-            // descriptionListBox
-            // 
-            this.descriptionListBox.FormattingEnabled = true;
-            this.descriptionListBox.Location = new System.Drawing.Point(347, 133);
-            this.descriptionListBox.Name = "descriptionListBox";
-            this.descriptionListBox.Size = new System.Drawing.Size(212, 316);
-            this.descriptionListBox.TabIndex = 8;
-            // 
             // closeButton
             // 
             this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.closeButton.Location = new System.Drawing.Point(210, 469);
+            this.closeButton.Location = new System.Drawing.Point(270, 424);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(155, 41);
             this.closeButton.TabIndex = 9;
@@ -134,16 +122,188 @@ namespace PipeliningSimulation
             this.closeButton.UseVisualStyleBackColor = true;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
+            // intructTypePanel
+            // 
+            this.intructTypePanel.Controls.Add(this.typeListBox3);
+            this.intructTypePanel.Controls.Add(this.typeListBox4);
+            this.intructTypePanel.Controls.Add(this.typeListBox2);
+            this.intructTypePanel.Controls.Add(this.typeListBox1);
+            this.intructTypePanel.Location = new System.Drawing.Point(38, 131);
+            this.intructTypePanel.Name = "intructTypePanel";
+            this.intructTypePanel.Size = new System.Drawing.Size(116, 273);
+            this.intructTypePanel.TabIndex = 10;
+            // 
+            // typeListBox3
+            // 
+            this.typeListBox3.FormattingEnabled = true;
+            this.typeListBox3.Location = new System.Drawing.Point(0, 136);
+            this.typeListBox3.Name = "typeListBox3";
+            this.typeListBox3.Size = new System.Drawing.Size(116, 43);
+            this.typeListBox3.TabIndex = 2;
+            // 
+            // typeListBox4
+            // 
+            this.typeListBox4.FormattingEnabled = true;
+            this.typeListBox4.Location = new System.Drawing.Point(0, 178);
+            this.typeListBox4.Name = "typeListBox4";
+            this.typeListBox4.Size = new System.Drawing.Size(116, 95);
+            this.typeListBox4.TabIndex = 3;
+            // 
+            // typeListBox2
+            // 
+            this.typeListBox2.FormattingEnabled = true;
+            this.typeListBox2.Location = new System.Drawing.Point(0, 94);
+            this.typeListBox2.Name = "typeListBox2";
+            this.typeListBox2.Size = new System.Drawing.Size(116, 43);
+            this.typeListBox2.TabIndex = 1;
+            // 
+            // typeListBox1
+            // 
+            this.typeListBox1.FormattingEnabled = true;
+            this.typeListBox1.Location = new System.Drawing.Point(0, 0);
+            this.typeListBox1.Name = "typeListBox1";
+            this.typeListBox1.Size = new System.Drawing.Size(116, 95);
+            this.typeListBox1.TabIndex = 0;
+            // 
+            // instructPanel
+            // 
+            this.instructPanel.Controls.Add(this.instructListBox3);
+            this.instructPanel.Controls.Add(this.instructListBox4);
+            this.instructPanel.Controls.Add(this.instructListBox1);
+            this.instructPanel.Controls.Add(this.instructListBox2);
+            this.instructPanel.Location = new System.Drawing.Point(153, 131);
+            this.instructPanel.Name = "instructPanel";
+            this.instructPanel.Size = new System.Drawing.Size(96, 273);
+            this.instructPanel.TabIndex = 11;
+            // 
+            // instructListBox3
+            // 
+            this.instructListBox3.FormattingEnabled = true;
+            this.instructListBox3.Location = new System.Drawing.Point(0, 136);
+            this.instructListBox3.Name = "instructListBox3";
+            this.instructListBox3.Size = new System.Drawing.Size(96, 43);
+            this.instructListBox3.TabIndex = 6;
+            // 
+            // instructListBox4
+            // 
+            this.instructListBox4.FormattingEnabled = true;
+            this.instructListBox4.Location = new System.Drawing.Point(0, 178);
+            this.instructListBox4.Name = "instructListBox4";
+            this.instructListBox4.Size = new System.Drawing.Size(96, 95);
+            this.instructListBox4.TabIndex = 7;
+            // 
+            // instructListBox1
+            // 
+            this.instructListBox1.FormattingEnabled = true;
+            this.instructListBox1.Location = new System.Drawing.Point(0, 0);
+            this.instructListBox1.Name = "instructListBox1";
+            this.instructListBox1.Size = new System.Drawing.Size(96, 95);
+            this.instructListBox1.TabIndex = 4;
+            // 
+            // instructListBox2
+            // 
+            this.instructListBox2.FormattingEnabled = true;
+            this.instructListBox2.Location = new System.Drawing.Point(0, 94);
+            this.instructListBox2.Name = "instructListBox2";
+            this.instructListBox2.Size = new System.Drawing.Size(96, 43);
+            this.instructListBox2.TabIndex = 5;
+            // 
+            // formatPanel
+            // 
+            this.formatPanel.Controls.Add(this.formatListBox3);
+            this.formatPanel.Controls.Add(this.formatListBox4);
+            this.formatPanel.Controls.Add(this.formatListBox1);
+            this.formatPanel.Controls.Add(this.formatListBox2);
+            this.formatPanel.Location = new System.Drawing.Point(246, 131);
+            this.formatPanel.Name = "formatPanel";
+            this.formatPanel.Size = new System.Drawing.Size(105, 273);
+            this.formatPanel.TabIndex = 11;
+            // 
+            // formatListBox3
+            // 
+            this.formatListBox3.FormattingEnabled = true;
+            this.formatListBox3.Location = new System.Drawing.Point(0, 136);
+            this.formatListBox3.Name = "formatListBox3";
+            this.formatListBox3.Size = new System.Drawing.Size(105, 43);
+            this.formatListBox3.TabIndex = 14;
+            // 
+            // formatListBox4
+            // 
+            this.formatListBox4.FormattingEnabled = true;
+            this.formatListBox4.Location = new System.Drawing.Point(0, 178);
+            this.formatListBox4.Name = "formatListBox4";
+            this.formatListBox4.Size = new System.Drawing.Size(105, 95);
+            this.formatListBox4.TabIndex = 15;
+            // 
+            // formatListBox1
+            // 
+            this.formatListBox1.FormattingEnabled = true;
+            this.formatListBox1.Location = new System.Drawing.Point(0, 0);
+            this.formatListBox1.Name = "formatListBox1";
+            this.formatListBox1.Size = new System.Drawing.Size(105, 95);
+            this.formatListBox1.TabIndex = 12;
+            // 
+            // formatListBox2
+            // 
+            this.formatListBox2.FormattingEnabled = true;
+            this.formatListBox2.Location = new System.Drawing.Point(0, 94);
+            this.formatListBox2.Name = "formatListBox2";
+            this.formatListBox2.Size = new System.Drawing.Size(105, 43);
+            this.formatListBox2.TabIndex = 13;
+            // 
+            // descPanel
+            // 
+            this.descPanel.Controls.Add(this.descListBox3);
+            this.descPanel.Controls.Add(this.descListBox4);
+            this.descPanel.Controls.Add(this.descListBox2);
+            this.descPanel.Controls.Add(this.descListBox1);
+            this.descPanel.Location = new System.Drawing.Point(350, 131);
+            this.descPanel.Name = "descPanel";
+            this.descPanel.Size = new System.Drawing.Size(318, 273);
+            this.descPanel.TabIndex = 11;
+            // 
+            // descListBox3
+            // 
+            this.descListBox3.FormattingEnabled = true;
+            this.descListBox3.Location = new System.Drawing.Point(0, 136);
+            this.descListBox3.Name = "descListBox3";
+            this.descListBox3.Size = new System.Drawing.Size(318, 43);
+            this.descListBox3.TabIndex = 6;
+            // 
+            // descListBox4
+            // 
+            this.descListBox4.FormattingEnabled = true;
+            this.descListBox4.Location = new System.Drawing.Point(0, 178);
+            this.descListBox4.Name = "descListBox4";
+            this.descListBox4.Size = new System.Drawing.Size(318, 95);
+            this.descListBox4.TabIndex = 7;
+            // 
+            // descListBox2
+            // 
+            this.descListBox2.FormattingEnabled = true;
+            this.descListBox2.Location = new System.Drawing.Point(0, 94);
+            this.descListBox2.Name = "descListBox2";
+            this.descListBox2.Size = new System.Drawing.Size(318, 43);
+            this.descListBox2.TabIndex = 5;
+            // 
+            // descListBox1
+            // 
+            this.descListBox1.FormattingEnabled = true;
+            this.descListBox1.Location = new System.Drawing.Point(0, 0);
+            this.descListBox1.Name = "descListBox1";
+            this.descListBox1.Size = new System.Drawing.Size(318, 95);
+            this.descListBox1.TabIndex = 4;
+            // 
             // InstructionReferenceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 535);
+            this.ClientSize = new System.Drawing.Size(704, 496);
+            this.Controls.Add(this.instructPanel);
+            this.Controls.Add(this.formatPanel);
+            this.Controls.Add(this.descPanel);
+            this.Controls.Add(this.intructTypePanel);
             this.Controls.Add(this.closeButton);
-            this.Controls.Add(this.descriptionListBox);
-            this.Controls.Add(this.formatListBox);
-            this.Controls.Add(this.instructListBox);
-            this.Controls.Add(this.intructTypeListBox);
             this.Controls.Add(this.formatLabel);
             this.Controls.Add(this.descLabel);
             this.Controls.Add(this.instructTypeLabel);
@@ -151,6 +311,10 @@ namespace PipeliningSimulation
             this.Controls.Add(this.formLabel);
             this.Name = "InstructionReferenceForm";
             this.Text = "Reference Page";
+            this.intructTypePanel.ResumeLayout(false);
+            this.instructPanel.ResumeLayout(false);
+            this.formatPanel.ResumeLayout(false);
+            this.descPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,10 +327,26 @@ namespace PipeliningSimulation
         private System.Windows.Forms.Label instructTypeLabel;
         private System.Windows.Forms.Label descLabel;
         private System.Windows.Forms.Label formatLabel;
-        private System.Windows.Forms.ListBox intructTypeListBox;
-        private System.Windows.Forms.ListBox instructListBox;
-        private System.Windows.Forms.ListBox formatListBox;
-        private System.Windows.Forms.ListBox descriptionListBox;
         private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Panel intructTypePanel;
+        private System.Windows.Forms.ListBox typeListBox3;
+        private System.Windows.Forms.ListBox typeListBox4;
+        private System.Windows.Forms.ListBox typeListBox2;
+        private System.Windows.Forms.ListBox typeListBox1;
+        private System.Windows.Forms.Panel instructPanel;
+        private System.Windows.Forms.Panel formatPanel;
+        private System.Windows.Forms.Panel descPanel;
+        private System.Windows.Forms.ListBox instructListBox3;
+        private System.Windows.Forms.ListBox instructListBox4;
+        private System.Windows.Forms.ListBox instructListBox1;
+        private System.Windows.Forms.ListBox instructListBox2;
+        private System.Windows.Forms.ListBox formatListBox3;
+        private System.Windows.Forms.ListBox formatListBox4;
+        private System.Windows.Forms.ListBox formatListBox1;
+        private System.Windows.Forms.ListBox formatListBox2;
+        private System.Windows.Forms.ListBox descListBox3;
+        private System.Windows.Forms.ListBox descListBox4;
+        private System.Windows.Forms.ListBox descListBox2;
+        private System.Windows.Forms.ListBox descListBox1;
     }
 }
--- PipeliningSimulation/InstructionReferenceForm.cs
+++ PipeliningSimulation/InstructionReferenceForm.cs
@@ -21,22 +21,63 @@ namespace PipeliningSimulation
 
         public void FillInBoxes()
         {
-            string[] instructionTypes = { "Data Transfer", "", "", "", "", "Arithmetic", "", "", "Control", "", "", "Floating Point", "", "", "" };
-            intructTypeListBox.Items.AddRange(instructionTypes);
+            //fills in the instruction types for the reference table
+            string[] strArray = { "Data Transfer" };
+            typeListBox1.Items.AddRange(strArray);
 
-            string[] instructions = { "lw", "flw", "sw", "fsw", "", "add", "sub", "", "beq", "bne", "", "fadd.s", "fsub.s", "fmul.s", "fdiv.s"};
-            instructListBox.Items.AddRange(instructions);
+            strArray = new string[] { "Arithmetic" };
+            typeListBox2.Items.AddRange(strArray);
 
-            string[] formats = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
-            formatListBox.Items.AddRange(formats);
+            strArray = new string[] { "Control" };
+            typeListBox3.Items.AddRange(strArray);
 
-            string[] descriptions = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
-            descriptionListBox.Items.AddRange(descriptions);
+            strArray = new string[] { "Floating Point" };
+            typeListBox4.Items.AddRange(strArray);
+
+            //fils in the instructions for the reference table
+            strArray = new string[] { "lw", "", "flw", "", "sw", "", "fsw"};
+            instructListBox1.Items.AddRange(strArray);
+
+            strArray = new string[] { "add", "", "sub" };
+            instructListBox2.Items.AddRange(strArray);
+
+            strArray = new string[] { "beq", "", "bne" };
+            instructListBox3.Items.AddRange(strArray);
+
+            strArray = new string[] { "fadd.s", "", "fsub.s", "", "fmul.s", "", "fdiv.s" };
+            instructListBox4.Items.AddRange(strArray);
+
+            //fills in the formats for the instructions for the reference table
+            strArray = new string[] { " lw rd, offset(rs1)", "", " flw rd, offset(rs1)", "", " sw rs2, offset(rs1)", "", " fsw rs2, offset(rs1)" };
+            formatListBox1.Items.AddRange(strArray);
+
+            strArray = new string[] { " add rd, rs1, rs2", "", " sub rd, rs1, rs2" };
+            formatListBox2.Items.AddRange(strArray);
+
+            strArray = new string[] { " beq rs1, rs2, offset", "", " bne rs1, rs2, offset" };
+            formatListBox3.Items.AddRange(strArray);
+
+            strArray = new string[] { " fadd.s rd, rs1, rs2", "", " fsub.s rd, rs1, rs2", "", " fmul.s rd, rs1, rs2", "", " fdiv.s rd, rs1, rs2" };
+            formatListBox4.Items.AddRange(strArray);
+
+            //fills in the decriptions of the instructions for the reference table
+            strArray = new string[] { "Load word from memory location into a register", "", "Load floating point number from memory location into a register", "", "Store word into memory location from a register", "", "Store floating point number into memory location from a register" };
+            descListBox1.Items.AddRange(strArray);
+
+            strArray = new string[] { "Add two values together and store in specified register", "", "Subtract two values and store in specified register" };
+            descListBox2.Items.AddRange(strArray);
+
+            strArray = new string[] { "Branch if equal to", "", "Branch if not equal to" };
+            descListBox3.Items.AddRange(strArray);
+
+            strArray = new string[] { "Floating point add", "", "Floating point subtract", "", "Floating point multiply", "", "Floating point divide" };
+            descListBox4.Items.AddRange(strArray);
         }
 
         private void closeButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
     }
 }
